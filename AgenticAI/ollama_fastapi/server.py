af9aa4e76dc0cfa3fdcaa62be78530cd5fd3cd42
Vipul Bhardwaj <vipulbhardwaj785@gmail.com>
2026-06-28 00:43:06 +0530

    Fast api integration with ollama

---
+++ AgenticAI/ollama_fastapi/server.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI, Body
+from ollama import Client
+
+app = FastAPI()
+
+client = Client(
+    host='http://localhost:11434/'
+)
+
+@app.get('/')
+def root_read():
+    return {'message':'Hello World'}
+
+@app.get('/contact-us')
+def read_root():
+    return {'email':'contactus@gmail.com'}
+
+@app.post('/chat')
+def chat(message: str = Body(..., description='The message')):
+    res = client.chat(
+        model='gemma2:2b',
+        messages=[
+            {'role':'user', 'content':message}
+        ]
+    )
+
+    return {'response': res.message.content}
